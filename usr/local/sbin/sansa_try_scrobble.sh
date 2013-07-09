08cee6438e2990689e04b28ea2df3545813aa4f2
Alexander Lopatin <sbar.geek@gmail.com>
2013-07-09 04:49:29 +0400

    update

--- usr/local/sbin/sansa_try_scrobble.sh
+++ usr/local/sbin/sansa_try_scrobble.sh
@@ -6,13 +6,23 @@ PLAYER_MOUNTPOINT="/media/CLIP_PLUS"
 SCRIPT_PATH="/root/git/Laspyt/laspyt.py"
 PROMPT="$0: ${PLAYER_MOUNTPOINT}"
 
-logger "${PROMPT} trying to scrobble"
+logger "${PROMPT} (1/2) trying to scrobble"
 sleep 10s
 
 ## save config only
 #python3 "$SCRIPT_PATH" -sc -f "$PLAYER_MOUNTPOINT/.scrobbler.log" -t +4 -u "$LASTFM_LOGIN" -p "$LASTFM_PASSWORD"
 
 python3 "$SCRIPT_PATH" -cb
+EXIT_STATUS=$?
+
+if [[ $EXIT_STATUS -eq 0 ]]; then
+    logger "${PROMPT} OK"
+else
+    logger "${PROMPT} FAIL"
+fi
+
+logger "${PROMPT} (2/2) trying to love"
+/usr/local/sbin/lastfm_love.py
 EXIT_STATUS=$?
 
 umount "$PLAYER_MOUNTPOINT"
