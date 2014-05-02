a0d22e7e139c5b07e019e458befaa91bcd1aca69
Alexander Lopatin <sbar.geek@gmail.com>
2014-05-02 04:51:13 +0400

    update; added a kernel hack patch for broadcom-sta proprietary driver

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -343,8 +343,8 @@ end
 clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
     awful.button({ modkey }, 1, awful.mouse.client.move),
-    awful.button({ modkey }, 2, awful.mouse.client.resize))
-    --awful.button({ modkey }, 3, awful.mouse.client.resize))
+    --awful.button({ modkey }, 2, awful.mouse.client.resize))
+    awful.button({ modkey }, 3, awful.mouse.client.resize))
 
 -- Set keys
 root.keys(globalkeys)
