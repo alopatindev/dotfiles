2cab255f6c5591befd2c7b460a5bf33fe6594bee
Alexander Lopatin <alopatindev@gmail.com>
2017-01-15 05:29:36 +0300

    switch-governor.sh: use no_turbo with powersave

--- desktop/usr/local/sbin/switch-governor.sh
+++ desktop/usr/local/sbin/switch-governor.sh
@@ -7,6 +7,11 @@ elif [[ $1 != $(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor) ]]; t
     for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
         echo -n "$1" > $i
     done
-    #cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
+    if [[ $1 == powersave ]]; then
+        echo -n 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+    else
+        echo -n 0 > /sys/devices/system/cpu/intel_pstate/no_turbo
+    fi
 fi
 
