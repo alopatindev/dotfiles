57631844793b492f5090d933a8258766e91c6d34
Alexander Lopatin <alopatindev@gmail.com>
2017-01-22 05:10:33 +0300

    update

--- common/home/scripts/set_touchpad.sh
+++ common/home/scripts/set_touchpad.sh
@@ -5,6 +5,6 @@ synclient HorizTwoFingerScroll=1 \
           FingerHigh=8 \
           TapButton1=0 TapButton2=0 TapButton3=0 \
           RBCornerButton=3 RTCornerButton=2 \
-          VertScrollDelta=135 HorizScrollDelta=135 \
+          VertScrollDelta=160 HorizScrollDelta=135 \
           MinSpeed=1.8 MaxSpeed=2 && \
 syndaemon -t -k -i 1 -d
