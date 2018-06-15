c5a0e26ea433c6f83b078d36f426cf5e6895e677
Alexander Lopatin <alopatindev@gmail.com>
2018-06-15 17:51:06 +0300

    update

--- common/home/scripts/exchange-rates.sh
+++ common/home/scripts/exchange-rates.sh
@@ -5,11 +5,14 @@ BTC=$(wget -qO - https://blockchain.info/ticker | jq -rM '.USD.buy')
 
 BCH=$(wget -qO - https://cex.io/api/ticker/BCH/USD | jq -rM '.last')
 
+ETH=$(wget -qO - https://cex.io/api/ticker/ETH/USD | jq -rM '.last')
+
 XVG=$(wget -qO - 'https://api.coinmarketcap.com/v1/ticker/verge/' | jq -rM '.[0].price_usd')
 
 USD=$(wget -qO - 'https://free.currencyconverterapi.com/api/v3/convert?q=USD_RUB' | jq -rM '.results.USD_RUB.val')
-#FB=$(wget -qO - 'https://query.yahooapis.com/v1/public/yql?q=select+%2A+from+yahoo.finance.quotes+where+symbol+%3D+%22FB%22&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&format=json' | jq -rM '.query.results.quote.LastTradePriceOnly')
+
+FB=$(wget -qO - https://finance.yahoo.com/quote/FB\?p\=FB | pup 'span[data-reactid=35] text{}' | head -n1)
 
 USD_CITI=$(wget -qO - https://www.citibank.ru/RUGCB/COA/frx/prefxratinq/flow.action | pup 'div[class=cT-columnMVRefCCYS] div[class=wwctrl] label text{}' | head -n18 | tail -n2 | grep -oE '[0-9.]*')
 
-printf 'B=%.2f/%2.f X=%.2f U=%.2f (%.2f)\n' ${BTC} ${BCH} ${XVG} ${USD} ${USD_CITI}
+printf 'B=%.2f/%2.f E=%.2f X=%.2f F=%.2f U=%.2f (%.2f)\n' ${BTC} ${BCH} ${ETH} ${XVG} ${FB} ${USD} ${USD_CITI}
