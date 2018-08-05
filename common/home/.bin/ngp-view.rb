794b88a92cfe769b25998e564753167827f29b9c
Alexander Lopatin <alopatindev@gmail.com>
2018-08-05 23:31:31 +0300

    fixes, refactoring

--- common/home/.bin/ngp-view.rb
+++ common/home/.bin/ngp-view.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
 
-line = ARGV[0].to_i
-filename = ARGV[1].gsub(/^\.\//, '')
-pattern = ARGV[2]
-jupyter_url = ARGV[3]
-jupyter_path = ARGV[4]
+require 'shellwords'
+
+BROWSER = ENV['BROWSER'] || 'chromium'
+EDITOR = ENV['EDITOR'] || 'nvim'
 
 ANCHOR_REGEX = /\s*#\s(.*?)\\n",$/
 
@@ -14,15 +13,25 @@ def parse_anchor(filename, line)
       .reverse
       .select { |line| line.match? ANCHOR_REGEX }
       .take(1)
-      .map { |line| line.match(ANCHOR_REGEX)[1].gsub(/^/, '#').tr(' ', '-') }[0] || ''
+      .map { |line| line.match(ANCHOR_REGEX)[1].sub(/^/, '#').tr(' ', '-') }[0] || ''
 end
+
+line = ARGV[0].to_i
+filename = ARGV[1].sub(/^\.\//, '')
+pattern = ARGV[2]
+jupyter_url = ARGV[3]
+jupyter_path = ARGV[4]
 
 file_extension = File.extname(filename)
 is_jupyter = file_extension == '.ipynb' && (Dir.pwd.start_with? jupyter_path)
-if is_jupyter
-  anchor = parse_anchor(filename, line)
-  jupyter_path = File.join(Dir.pwd, filename).gsub(/^#{jupyter_path}/, '')
-  system("chromium '#{jupyter_url}#{jupyter_path}#{anchor}'")
-else
-  system("nvim -c '/#{pattern}' -c '#{line}' '#{filename}'")
-end
+command =
+  if is_jupyter
+    anchor = parse_anchor(filename, line)
+    jupyter_path = File.join(Dir.pwd, filename).sub(/^#{jupyter_path}/, '')
+    url = "#{jupyter_url}#{jupyter_path}#{anchor}"
+    [BROWSER, url]
+  else
+    [EDITOR, '-c', "/#{pattern}", '-c', line, filename]
+  end
+
+system Shellwords.join(command)
