76c348c4429568fd23f5f0174e8b52be90a0391f
Alexander Lopatin <alopatindev@gmail.com>
2019-12-31 13:05:22 +0300

    update

--- lenlap/usr/local/sbin/set-alarm-clock-and-standby.rb
+++ lenlap/usr/local/sbin/set-alarm-clock-and-standby.rb
@@ -10,6 +10,7 @@ DISPLAY_SIZE = `xdpyinfo | awk '/dimensions:/ { print $2; exit }'`.strip.split('
 BACKLIGHT_PATH = '/sys/class/backlight/intel_backlight'.freeze
 BRIGHTNESS_PATH = "#{BACKLIGHT_PATH}/brightness".freeze
 MAX_BRIGHTNESS = `cat #{BACKLIGHT_PATH}/max_brightness`.to_i
+RTC_ACCURACY_SECS = 10
 
 def interpolate(start_color, end_color, start_time, end_time, time)
   delta_color = end_color - start_color
@@ -38,12 +39,14 @@ def set_brightness(brightness)
   brightness
 end
 
-next_time = "today #{FINISH_TIME} - #{SUNRISE_TIME_SECS} seconds"
-if `date +%s` >= `date +%s -d "#{next_time} - #{SUNRISE_TIME_SECS} seconds"`
-  next_time = "tomorrow #{FINISH_TIME} - #{SUNRISE_TIME_SECS} seconds"
+wake_time = "today #{FINISH_TIME} - #{SUNRISE_TIME_SECS} seconds"
+print [`date +%s`.to_i, `date +%s -d "#{wake_time} - #{SUNRISE_TIME_SECS} seconds"`.to_i]
+if `date +%s`.to_i >= `date +%s -d "#{wake_time} - #{SUNRISE_TIME_SECS} seconds"`.to_i
+  wake_time = "tomorrow #{FINISH_TIME} - #{SUNRISE_TIME_SECS} seconds"
 end
+wake_time_timestamp = `date +%s -d "#{wake_time}"`.to_i
 
-system "rtcwake -m no -l -t $(date +%s -d '#{next_time}') ; logger 'set wakeup timer to #{next_time}'"
+system "rtcwake -m no -l -t $(date +%s -d '#{wake_time}') ; logger 'set wakeup timer to #{wake_time}'"
 
 colors = POINTS.each_cons(2).flat_map do |start_point, end_point|
   (0..SUNRISE_TIME_PER_POINT).map do |time|
@@ -55,16 +58,18 @@ initial_brightness = get_brightness
 brightness = set_brightness(0)
 
 system '/usr/local/sbin/standby'
-set_brightness(0)
 
-brightness_step = MAX_BRIGHTNESS / SUNRISE_TIME_SECS
-make_image(0, 0, 0)
-system "X :#{XDISPLAY} & X_PID=$! && sleep 0.5s && DISPLAY=:#{XDISPLAY} feh -R 1 --scale-down --auto-zoom #{PIC_PATH} && kill ${X_PID} && echo -n #{initial_brightness} >> #{BRIGHTNESS_PATH} &"
+current_time_timestamp = `date +%s`.to_i
+if current_time_timestamp <= wake_time_timestamp - RTC_ACCURACY_SECS || current_time_timestamp <= wake_time_timestamp + RTC_ACCURACY_SECS
+  brightness_step = MAX_BRIGHTNESS / SUNRISE_TIME_SECS
+  make_image(0, 0, 0)
+  system "X :#{XDISPLAY} & X_PID=$! && sleep 0.5s && DISPLAY=:#{XDISPLAY} feh -R 1 --scale-down --auto-zoom #{PIC_PATH} && kill ${X_PID} && echo -n #{initial_brightness} >> #{BRIGHTNESS_PATH} &"
 
-for r, g, b in colors
-  make_image(r, g, b)
-  set_brightness(get_brightness + brightness_step)
-  sleep 1
+  for r, g, b in colors
+    make_image(r, g, b)
+    set_brightness(get_brightness + brightness_step)
+    sleep 1
+  end
 end
 
 system 'echo -n 0 > /sys/class/rtc/rtc0/wakealarm'
