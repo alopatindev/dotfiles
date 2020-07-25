4e48fc696de3973955f5e211c7095eb6368b9e7b
Alexander Lopatin <alopatindev@gmail.com>
2020-07-26 00:21:16 +0300

    add notifications

--- common/home/scripts/co2mond-status.sh
+++ common/home/scripts/co2mond-status.sh
@@ -3,6 +3,27 @@
 DATA_DIR="/tmp/co2mond"
 CO2_PATH="${DATA_DIR}/CntR"
 TEMP_PATH="${DATA_DIR}/Tamb"
+LAST_NOTIFICATION_TIME_PATH="${DATA_DIR}/notification_time"
+PAUSE_BETWEEN_NOTIFICATIONS_IN_MINS=10
+
+YELLOW_ZONE=800
+RED_ZONE=1200
+
+maybe_notify () {
+    current_timestamp=$(date +%s)
+    priority="$1"
+    text="$2"
+    mins_since_last_notification=$(echo "(${current_timestamp} - $(cat ${LAST_NOTIFICATION_TIME_PATH})) / 60" | bc)
+    mins_since_last_notification=$(printf "%.0f" "${mins_since_last_notification}")
+    if [ "${mins_since_last_notification}" -gt "${PAUSE_BETWEEN_NOTIFICATIONS_IN_MINS}" ]; then
+        notify-send -t 0 -u "${priority}" "${text}"
+        echo "${current_timestamp}" > "${LAST_NOTIFICATION_TIME_PATH}"
+    fi
+}
+
+if [ ! -f "${LAST_NOTIFICATION_TIME_PATH}" ]; then
+    echo 0 > "${LAST_NOTIFICATION_TIME_PATH}"
+fi
 
 if [ -f "${TEMP_PATH}" ]; then
     TEMP=$(cat "${TEMP_PATH}")
@@ -13,11 +34,13 @@ if [ -f "${CO2_PATH}" ]; then
     CO2=$(cat "${CO2_PATH}")
     echo -n "CO2 ${CO2} PPM"
 
-    if [ ${CO2} -lt 800 ]; then
+    if [ "${CO2}" -lt "${YELLOW_ZONE}" ]; then
         echo ' (normal)'
-    elif [ ${CO2} -lt 1200 ]; then
-        echo ' o_o time to open the window!'
+    elif [ "${CO2}" -lt "${RED_ZONE}" ]; then
+        echo ' (WARNING)'
+        maybe_notify normal ' o_o time to open the window!'
     else
-        echo ' O_O OMG! open the window, now!!!'
+        echo ' (CRITICAL)'
+        maybe_notify critical ' O_O TIME TO OPEN THE WINDOW!'
     fi
 fi
