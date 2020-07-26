695e604b1cc4657ec828f7f43e5a544271377c73
Alexander Lopatin <alopatindev@gmail.com>
2020-07-26 16:52:30 +0300

    update

--- common/home/scripts/co2mond-status.sh
+++ common/home/scripts/co2mond-status.sh
@@ -13,8 +13,7 @@ maybe_notify () {
     current_timestamp=$(date +%s)
     priority="$1"
     text="$2"
-    mins_since_last_notification=$(echo "(${current_timestamp} - $(cat ${LAST_NOTIFICATION_TIME_PATH})) / 60" | bc)
-    mins_since_last_notification=$(printf "%.0f" "${mins_since_last_notification}")
+    mins_since_last_notification=$((( (${current_timestamp} - $(cat ${LAST_NOTIFICATION_TIME_PATH})) / 60 )))
     if [ "${mins_since_last_notification}" -gt "${PAUSE_BETWEEN_NOTIFICATIONS_IN_MINS}" ]; then
         notify-send -t 0 -u "${priority}" "${text}"
         echo "${current_timestamp}" > "${LAST_NOTIFICATION_TIME_PATH}"
