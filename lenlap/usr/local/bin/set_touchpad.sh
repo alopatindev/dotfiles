44d64a71440707c9fd39dc10a3a00abf27aa62ad
Alexander Lopatin <alopatindev@gmail.com>
2020-10-02 22:55:09 +0300

    tune touchpad

--- lenlap/usr/local/bin/set_touchpad.sh
+++ lenlap/usr/local/bin/set_touchpad.sh
@@ -9,10 +9,11 @@ if lsusb -d 046d: ; then
               FingerHigh=8 \
               TapButton1=0 TapButton2=0 TapButton3=0 \
               RBCornerButton=3 RTCornerButton=2 \
-              VertScrollDelta=160 HorizScrollDelta=135 \
-              MinSpeed=1.8 MaxSpeed=2 \
+              VertScrollDelta=135 HorizScrollDelta=135 \
+              MinSpeed=2.2 MaxSpeed=2.5 \
               RightButtonAreaLeft=0 RightButtonAreaTop=0 \
-              RTCornerButton=0 RBCornerButton=0
+              RTCornerButton=0 RBCornerButton=0 && \
+    syndaemon -t -k -i 1 -d
 else
     logger 'using lenovo ideapad y700 touchpad'
     synclient HorizTwoFingerScroll=1 \
@@ -23,5 +24,6 @@ else
               VertScrollDelta=160 HorizScrollDelta=135 \
               MinSpeed=1.8 MaxSpeed=2 \
               RightButtonAreaLeft=0 RightButtonAreaTop=0 \
-              RTCornerButton=0 RBCornerButton=0
-fi && syndaemon -t -k -i 1 -d
+              RTCornerButton=0 RBCornerButton=0 && \
+    syndaemon -t -k -i 1 -d
+fi
