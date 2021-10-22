86e6e594d21b1d7042a7b3eda0d554dcce07b78e
Alexander Lopatin <alopatindev@gmail.com>
2021-10-23 00:37:54 +0300

    update

---
+++ lenlap/usr/local/sbin/bluetooth-off.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+logger 'bluetooth OFF'
+bluetoothctl power off
---
+++ lenlap/usr/local/sbin/bluetooth-on.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+bluetoothctl power off
+
+# https://github.com/jkulesza/usbreset
+# https://marc.info/?l=linux-usb&m=121459435621262
+usbreset $(lsusb -d 0cf3: | perl -nE "/\D+(\d+)\D+(\d+).+/; print qq(/dev/bus/usb/\$1/\$2)")
+
+logger 'bluetooth ON'
+bluetoothctl power on
