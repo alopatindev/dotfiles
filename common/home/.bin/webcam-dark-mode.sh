ba8e0d4b5ef5ac9158c3836d74ec64f3a567dd95
Alexander Lopatin <alopatindev@gmail.com>
2021-11-11 02:59:08 +0300

    block some usb devices

--- common/home/.bin/webcam-dark-mode.sh
+++ common/home/.bin/webcam-dark-mode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /usr/local/sbin/reset-webcam.sh
-v4l2-ctl --verbose --device=/dev/video2 --set-ctrl=saturation=92 --set-ctrl=gain=65
+v4l2-ctl --verbose --device=/dev/video0 --set-ctrl=saturation=92 --set-ctrl=gain=65
--- common/home/.bin/webcam-night-mode.sh
+++ common/home/.bin/webcam-night-mode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /usr/local/sbin/reset-webcam.sh
-v4l2-ctl --verbose --device=/dev/video2 --set-ctrl=saturation=85 --set-ctrl=gain=155
+v4l2-ctl --verbose --device=/dev/video0 --set-ctrl=saturation=85 --set-ctrl=gain=155
