655c5e2fcdc21676b178138375ddd663574b9e64
Alexander Lopatin <alopatindev@gmail.com>
2021-11-12 20:31:31 +0300

    update

--- common/home/scripts/tmux-copy.rb
+++ common/home/scripts/tmux-copy.rb
@@ -7,7 +7,8 @@ clipboards = %w[clipboard]
 buffer = STDIN.read
 
 clipboards.each do |i|
-  Open3.popen3("xsel -i --#{i}") do |stdin, _stdout, _stderr, _wait_thr|
+  system("xsel --clear --#{i}")
+  Open3.popen3("xsel --input --#{i}") do |stdin, _stdout, _stderr, _wait_thr|
     stdin.write buffer
     stdin.close
   end
