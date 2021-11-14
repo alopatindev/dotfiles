19fac458fdf36709474af8831460c8df486b30ec
Alexander Lopatin <alopatindev@gmail.com>
2021-11-14 16:13:31 +0300

    update

--- common/home/.bin/webcam-dark-mode.sh
+++ common/home/.bin/webcam-dark-mode.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-/usr/local/sbin/reset-webcam.sh
 v4l2-ctl --verbose --device=/dev/video0 --set-ctrl=saturation=92 --set-ctrl=gain=65
--- common/home/.bin/webcam-night-mode.sh
+++ common/home/.bin/webcam-night-mode.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-/usr/local/sbin/reset-webcam.sh
 v4l2-ctl --verbose --device=/dev/video0 --set-ctrl=saturation=85 --set-ctrl=gain=155
