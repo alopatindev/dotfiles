00bd60f9484fd59d18bb759d6e6840a0c259cda0
Alexander Lopatin <alopatindev@gmail.com>
2021-12-31 16:49:18 +0300

    update

--- common/home/scripts/tmux-cd-to-current-dir.sh
+++ common/home/scripts/tmux-cd-to-current-dir.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd $(tmux display-message -p -F "#{pane_current_path}" -t0 | sed 's!'${HOME}'/.private/work/!'${HOME}'/work/!')
+cd "$(tmux display-message -p -F '#{pane_current_path}' -t0 | sed 's!'${HOME}'/.private/work/!'${HOME}'/work/!')"
