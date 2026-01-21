614241a623fe47af358e2387d6862c48e3fda32b
Alexander Lopatin <alopatindev@codonaft.com>
2026-01-21 19:19:05 +0400

    update

--- common/home/scripts/uptime.sh
+++ common/home/scripts/uptime.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+ping -c1 1.1.1.1 >>/dev/null || {
+  echo 'NO NETWORK'
+  exit
+}
+
 for i in media.codonaft mirror.codonaft ; do
   output=$(ssh "$i" -- uptime --pretty 2>>/dev/stdout)
   [[ "$?" -ne 0 ]] && notify-send -t 60000 "$i: $output"
