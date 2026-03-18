c385fbd34f5cad440a2c5ed6859a82b9a4fd24ca
Alex <alopatindev@codonaft.com>
2026-03-19 02:41:54 +0400

    Update

--- common/home/scripts/uptime.sh
+++ common/home/scripts/uptime.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+msg () {
+  printf "%17s: %s\n" "$1" "$2"
+}
+
 ping -c1 1.1.1.1 >>/dev/null || {
   echo 'NO NETWORK'
   exit
 }
 
-for i in media.codonaft mirror.codonaft ; do
+for i in $(grep '\.codonaft' /etc/hosts | awk '{ print $2 }') ; do
+  ps uax | grep ssh: | grep "$i" | grep '\[mux\]' | grep -v grep >>/dev/null || {
+    msg "$i" "is NOT connected"
+    continue
+  }
   output=$(ssh "$i" -- uptime --pretty 2>>/dev/stdout)
   [[ "$?" -ne 0 ]] && notify-send -t 60000 "$i: $output"
-  printf "%17s: %s\n" "$i" "${output}"
+  msg "$i" "${output}"
 done
